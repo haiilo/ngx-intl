e2f0ee58c9885bd7f0ec9550240c447aadf1f17e
Fynn Feldpausch <fynn.feldpausch@coyoapp.com>
2022-08-08 15:57:39 +0200

    fix: add new pipes to public api

--- projects/ngx-intl/src/public-api.ts
+++ projects/ngx-intl/src/public-api.ts
@@ -3,6 +3,8 @@
  */
 
 export * from './lib/intl-date.pipe';
+export * from './lib/intl-list.pipe';
 export * from './lib/intl-number.pipe';
 export * from './lib/intl-plural.pipe';
 export * from './lib/intl-sort.pipe';
+export * from './lib/intl-timeago.pipe';
