0454be25a438e9aef3dbeb91ddd5b74a17c0c36b
Fynn Feldpausch <fynn.feldpausch@coyoapp.com>
2023-05-26 11:25:32 +0200

    chore: fix tests

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { IntlTimeagoPipe } from 'ngx-intl';
+import { IntlTimeagoPipe } from 'projects/ngx-intl/src/lib/intl-timeago.pipe';
 
 @NgModule({
   imports: [BrowserModule, IntlTimeagoPipe],
